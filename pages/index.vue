9baca72a00b5f05c71311c9660b430be2a16c0ac
Justin Hayes <justin@justinhayes.me>
2023-03-13 22:19:25 -0400

    Improving index page copy.

--- pages/index.vue
+++ pages/index.vue
@@ -14,11 +14,31 @@
         <VCol cols="12" sm="7">
         <section>
         <h2>Description</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed arcu lorem, luctus quis efficitur at, varius sit amet massa. Sed ut ante id lorem feugiat sodales sit amet ac nulla. Vivamus non metus quis sapien sagittis viverra ut et tortor. Mauris in nisl nec turpis eleifend vestibulum. Sed tristique gravida mi ut rhoncus. Etiam bibendum non ex a malesuada. Duis tempor luctus dictum. Pellentesque lobortis eget leo non mattis. Sed non mattis erat. Duis pretium tellus turpis, at varius velit vestibulum et. Nam tempus turpis at magna euismod volutpat.</p>
-        </section>
-        <section>
-        <h2>Section 2</h2>
-        <p>Sed lobortis, nulla a tincidunt pretium, felis augue eleifend nibh, non tincidunt mauris eros eget risus. Vivamus molestie rhoncus fermentum. Aliquam laoreet nisl dolor, nec venenatis est vehicula sed. Nullam bibendum, elit non elementum porttitor, dolor enim scelerisque est, eget sollicitudin tellus nibh id neque. Proin lacus ipsum, venenatis tempus quam vel, semper ullamcorper sapien. Etiam consequat sapien id vestibulum condimentum. Sed tempor felis sapien, nec dictum mi tempus id. Pellentesque ornare venenatis turpis eu gravida.</p>
+        <p>Ελλάδα is an example of a simple full-stack <a href="https://www.typescriptlang.org/">TypeScript</a> application. The main feature is a <NuxtLink to="/artifact">browser</NuxtLink> for the subset
+            of the Metropolitan Museum of Art's <a href="https://github.com/metmuseum/openaccess">collections</a> related to ancient Greece. The browser allows the user to page through the collection
+            and filter the results by a combination of historical period, classification of the material, and thematic tag.
+        </p>
+        <h3>Frontend</h3>
+        <p>The frontend application uses the following technologies:</p>
+        <ul>
+            <li><a href="https://vuejs.org/">Vue 3</a> + <a href="https://nuxt.com/">Nuxt 3</a></li>
+            <li><a href="https://vuetifyjs.com/en/">Vuetify 3</a></li>
+            <li><a href="https://vitejs.dev/">Vite</a></li>
+        </ul>
+        <p>Artifact images are sourced from the MET's <a href="https://metmuseum.github.io/">API</a> and served from a <a href="https://aws.amazon.com/cloudfront/">CloudFront</a> distribution from <a href="https://aws.amazon.com/pm/serv-s3">Amazon S3</a>.</p>
+        <p>On commit, a <a href="https://github.com/features/actions">GitHub action</a> builds the frontend application in client-side rendering mode and deploys the static files
+            to the repository's GitHub Pages site.
+        </p>
+        <h3>Backend</h3>
+        <p>The backend application implements a RESTful API using the following technologies:</p>
+        <ul>
+            <li><a href="https://nestjs.com/">NestJS</a></li>
+            <li><a href="https://www.openapis.org/">OpenAPI</a></li>
+            <li><a href="https://jwt.io/">JWT</a></li>
+            <li><a href="https://typeorm.io/">TypeORM</a></li>
+            <li><a href="https://www.postgresql.org/">Postgres</a></li>
+        </ul>
+        <p>The application server and Postgres instance run on the <a href="https://fly.io/">Fly.io</a> platform.</p>
         </section>
         </VCol>
         <VCol cols="12" sm="5">
@@ -36,5 +56,14 @@
     .main {
         max-width: 50vw;
     }
+}
+p {
+    margin-bottom: 5px;
+}
+ul {
+    margin-bottom: 10px;
+}
+li {
+    margin-left: 20px;
 }
 </style>
